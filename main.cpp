bd24f66419524873fbedebe358abbef5ed65fcdf
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-28 01:19:04 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// Hoppock, Zachary
+// whileLoop.cpp
+// 10/8/2020
+// Program with do a while loop on how many numbers the variable "I" is
+// Version # 1
+#include <iostream>
+using namespace std;
+
+int main()
+{ 
+  int I = 10;
+  while (I < 20)
+  {
+    cout << "The value of I is " << I << endl;
+    I++;
+  }
+  return 0;
+}
